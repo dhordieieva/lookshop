afdf8d4ec78fff58702417e61511084d74ffd59e
dhordieieva <dasha.gordeeva95@gmail.com>
2019-09-27 15:41:29 +0300

    Add lists to the Navigaton and add styles for mobile view of the Header

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,8 @@
 $(function(){
+  $('.navigation-hamburger').on('click', function (event) {
+    var $this = $(event.currentTarget);
+    var $nav = $this.parent();
 
+    $nav.toggleClass('open');
+  })
 });
