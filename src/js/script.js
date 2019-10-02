69f5badf432e1bb62e56d7ad9a729ed07b47dc33
dhordieieva <dasha.gordeeva95@gmail.com>
2019-10-03 00:00:35 +0300

    Add the Main Slider on the Home page

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,29 @@ $(function(){
     var $nav = $this.parent();
 
     $nav.toggleClass('open');
-  })
-});
+  });
 
-$(function(){
   $('.pre-header-search__icon').on('click', function (event) {
     var $this = $(event.currentTarget);
     var $parent = $this.parent();
 
     $parent.toggleClass('open');
-  })
+  });
+
+  var swiper = new Swiper('.main-slider', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    speed: 1000,
+    loop: true,
+    pagination: {
+      el: '.main-slider__pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.main-slider__btn--next',
+      prevEl: '.main-slider__btn--prev',
+    },
+  });
+
+
 });
