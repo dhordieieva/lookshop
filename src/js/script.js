fe499a9e5bc1a3090bff0ac17ef5bdbfb35760da
dhordieieva <dasha.gordeeva95@gmail.com>
2019-10-09 22:33:49 +0300

    Add the Offers add the Devider blocks on the Home page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
   $('.navigation-hamburger').on('click', function (event) {
     var $this = $(event.currentTarget);
     var $nav = $this.parent();
@@ -28,63 +28,70 @@ $(function(){
     },
   });
 
-  var offerHot = new Swiper('.offer-slider-hot', {
-    slidesOffsetBefore: 120,
-    slidesPerView: 5,
-    spaceBetween: 30,
-    centeredSlides: true,
-    speed: 300,
-    loop: true,
-    navigation: {
-      nextEl: '.offer-slider__btn--next-hot',
-      prevEl: '.offer-slider__btn--prev-hot',
-    },
-  });
+  function generateOfferSlider(sliderClass, btnPrevClass, btnNextClass) {
+    return new Swiper(sliderClass, {
+      slidesPerView: 2,
+      spaceBetween: 30,
+      speed: 300,
+      loop: true,
+      navigation: {
+        nextEl: btnPrevClass,
+        prevEl: btnNextClass,
+      },
+      breakpoints: {
+        641: {
+          slidesPerView: 3,
+        },
+        769: {
+          slidesPerView: 4,
+        },
+        993: {
+          slidesPerView: 5,
+          centeredSlides: true,
+          slidesOffsetBefore: 0,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+          slidesOffsetBefore: 120,
+        }
+      }
+    });
+  }
 
-  var offerDesigners = new Swiper('.offer-slider-designers', {
-    slidesOffsetBefore: 120,
-    slidesPerView: 5,
-    spaceBetween: 30,
-    centeredSlides: true,
-    speed: 300,
-    loop: true,
-    navigation: {
-      nextEl: '.offer-slider__btn--next-designers',
-      prevEl: '.offer-slider__btn--prev-designers',
-    },
-  });
+  var offerHot = generateOfferSlider(
+    '.offer-slider-hot',
+    '.offer-slider__btn--next-hot',
+    '.offer-slider__btn--prev-hot');
 
-  var offerFeatured = new Swiper('.offer-slider-featured', {
-    slidesOffsetBefore: 120,
-    slidesPerView: 5,
-    spaceBetween: 30,
-    centeredSlides: true,
-    speed: 300,
-    loop: true,
-    navigation: {
-      nextEl: '.offer-slider__btn--next-featured',
-      prevEl: '.offer-slider__btn--prev-featured',
-    },
-  });
+  var offerDesigners = generateOfferSlider(
+    '.offer-slider-designers',
+    '.offer-slider__btn--next-designers',
+    '.offer-slider__btn--prev-designers');
 
-  var offerLatest = new Swiper('.offer-slider-latest', {
-    slidesOffsetBefore: 120,
-    slidesPerView: 5,
-    spaceBetween: 30,
-    centeredSlides: true,
-    speed: 300,
-    loop: true,
-    navigation: {
-      nextEl: '.offer-slider__btn--next-latest',
-      prevEl: '.offer-slider__btn--prev-latest',
-    },
-  });
+  var offerFeatured = generateOfferSlider(
+    '.offer-slider-featured',
+    '.offer-slider__btn--next-featured',
+    '.offer-slider__btn--prev-featured');
 
-  $('.offers .nav-tabs__link').on('shown.bs.tab', function (e) {
+
+  var offerLatest = generateOfferSlider(
+    '.offer-slider-latest',
+    '.offer-slider__btn--next-latest',
+    '.offer-slider__btn--prev-latest');
+
+  function updateTabsOfferSliders() {
     offerHot.update();
     offerDesigners.update();
     offerFeatured.update();
     offerLatest.update();
-  })
+  }
+
+  $('.nav-tabs__link').on('shown.bs.tab', updateTabsOfferSliders);
+  $('.nav-tab-mobile__link').on('shown.bs.tab', updateTabsOfferSliders);
+  $('.nav-tab-mobile__link').on('click', function (event) {
+    var $this = $(event.currentTarget);
+    $('.nav-tab-mobile__dropdown').find('.dropdown-toggle__text').text($this.text());
+  });
 
 });
